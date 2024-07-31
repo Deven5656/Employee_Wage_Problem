d08224c002c12702bb6342dde33c9dc1e0e88d5f
Deven5656 <devengupta0987@gmail.com>
2024-07-31 15:27:40 +0530

    [Deven] Add: Python program for Employee Wage Computation

---
+++ emp.py
@@ -0,0 +1,9 @@
+""" 
+    @Author: Deven Gupta
+    @Date: 31-07-2024
+    @Last Modified by: Deven Gupta
+    @Last Modified time: 31-07-2024
+    @Title : Python program for Employee Wage Computation
+    
+"""   
+print("Welcome to Employee Wage Computation Program")
