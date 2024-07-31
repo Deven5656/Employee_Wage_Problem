b2fb37b843f9c2b37484052f3e06ee99e97dcc16
Deven5656 <devengupta0987@gmail.com>
2024-07-31 15:42:55 +0530

    [Deven] Add: Python program to Check attendance of employee

--- emp.py
+++ emp.py
@@ -3,7 +3,28 @@
     @Date: 31-07-2024
     @Last Modified by: Deven Gupta
     @Last Modified time: 31-07-2024
-    @Title : Python program for Employee Wage Computation
-    
+    @Title : UC-1 Python program to Check attendance of employee
+
 """   
-print("Welcome to Employee Wage Computation Program")
+print("Welcome to Employee Wage Computation Program\n")
+
+import random
+
+def check_attendance():
+    """
+    Description:
+        This function used to Check attendance of employee
+    Parameters:
+        None
+    Returns:
+        None
+        
+    """
+    status = random.choice([0,1])
+    if status == 1 :
+        print("Employee is Present")
+    else:
+        print("Employee is Absent")
+
+if __name__ == "__main__" :
+    check_attendance()
