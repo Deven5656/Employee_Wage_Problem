1a34258396b32cb3767f42ce045379dbcf44fe3d
Deven5656 <devengupta0987@gmail.com>
2024-07-31 15:59:46 +0530

    [Deven] Add : Python program to Calculate Wage

--- emp.py
+++ emp.py
@@ -3,12 +3,15 @@
     @Date: 31-07-2024
     @Last Modified by: Deven Gupta
     @Last Modified time: 31-07-2024
-    @Title : UC-1 Python program to Check attendance of employee
+    @Title : UC-2 Python program to Calculate Wage
 
 """   
 print("Welcome to Employee Wage Computation Program\n")
 
 import random
+WAGE_PER_HOUR = 20
+FULL_TIME_HOUR = 8
+
 
 def check_attendance():
     """
@@ -17,14 +20,37 @@ def check_attendance():
     Parameters:
         None
     Returns:
-        None
+        boolean : True if present else False(absent)
         
     """
     status = random.choice([0,1])
     if status == 1 :
-        print("Employee is Present")
+        print("Employee Present")
+        return True
     else:
-        print("Employee is Absent")
+        print("Employee Absent")
+        return False
+
+
+def cal_daily_wage():
+    """
+    Description:
+        This function used to calculate the daily wage
+    Parameters:
+        None
+    Returns:
+        int : daily wage
+
+    """
+    daily_wage = 0
+    if check_attendance():
+        daily_wage = WAGE_PER_HOUR * FULL_TIME_HOUR
+        return daily_wage
+    return daily_wage
+
+def main():
+    print(f"The Daily wage of Employee is {cal_daily_wage()}")
+
 
 if __name__ == "__main__" :
-    check_attendance()
+    main()
